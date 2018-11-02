4cdf382e2444c9d51b2d46fb34d780bcbd9cd210
Jakub Steiner <jimmac@gmail.com>
2018-11-02 17:46:22 +0100

    update app icon
    See https://gitlab.gnome.org/GNOME/Initiatives/issues/2 for more info

--- icons.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env zsh
-app="technology.unrelenting.galacritty"
-for i in 16 24 32 48 64 128 256; do
-    rsvg-convert -w $i -h $i res/icons/hicolor/scalable/apps/$app.svg \
-        | pngquant --quality=70-90 - > res/icons/hicolor/${i}x${i}/apps/$app.png
-done
